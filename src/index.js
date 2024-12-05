e833a40a911fe468e142275f665e2ea9bea94792
becca-ops <rebecca.a.laffan@ul.ie>
2024-12-05 15:53:40 +0000

    Improved ux

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ function generateSong(event) {
     "You are a songwriter with a very similar style to Taylor Swift's lyric writing abilities. Your mission is to generate a song with two verses, chorus and bridge. Make sure each line is separated by <br /> and each verse, chorus, bridge should be inside a <strong> element. The title and each verse, chorus and bridge should be inside its own paragraph (<p>). Give the song a title as well, but don't include the word 'title:'. Make sure to keep the result similar to Taylor's writing style and lyricism but avoid plagiarism please. Ensure you follow user instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let songElement = document.querySelector("#song");
+  songElement.innerHTML = `<div class="blink_me"> ⏳ Generating your very own Taylor Swift song about ${instructionsInput.value}</div>`;
+
   console.log("Generating song");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
